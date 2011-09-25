06e790b518856b274c734370d0cc83d7bf54d9bd
Peter Sobot <psobot@gmail.com>
2011-09-25 10:20:54 -0400

    Fixed remixer behaviour

--- remixers/beatbox.py
+++ remixers/beatbox.py
@@ -32,9 +32,9 @@ def are_snares(x):
 
 def are_hats(x):
     loud = x.timbre[0] < 45
-    bright = x.timbre[1] > 90
-    flat =  x.timbre[2] < 0
-    attack = x.timbre[3] > 70
+    bright = x.timbre[1] > 80
+    flat =  x.timbre[2] < 10
+    attack = x.timbre[3] > 60
     what = x.timbre[4] < 40
     return loud and bright and flat and attack and what
 
@@ -54,43 +54,44 @@ class Beatbox(Remixer):
         self.original = audio.LocalAudioFile(self.infile)
         self.tag['tempo'] = self.original.analysis.tempo
 
-        self.log("Sorting kicks and snares...", 10)
         kicks = self.original.analysis.segments.that(are_kicks)
         snares = self.original.analysis.segments.that(are_snares)
         hats = self.original.analysis.segments.that(are_hats)
 
         # Time to replace
-        self.log("Replacing beats...", 20)
+        self.log("Replacing beats...", 30)
         hat_sample = audio.AudioData(self.sample_path + self.template['hats'], sampleRate=44100, numChannels=2, verbose=False)
         kick_sample = audio.AudioData(self.sample_path + self.template['kick'], sampleRate=44100, numChannels=2, verbose=False)
         snare_sample = audio.AudioData(self.sample_path + self.template['snare'], sampleRate=44100, numChannels=2, verbose=False)
   
-        empty = audio.AudioData(ndarray=numpy.zeros(((self.original.sampleRate * self.original.analysis.duration), 2), dtype=numpy.int16), numChannels=2, sampleRate=44100)
-
         last = 0
         for segment in kicks:
             if last + len(kick_sample.data) > segment.start:
                 truncated = kick_sample.data
                 if self.original.sampleRate*segment.start + len(kick_sample.data) > len(self.original.data):
                     truncated = truncated[:len(empty.data[self.original.sampleRate*segment.start:self.original.sampleRate*segment.start + len(kick_sample.data)])]
-                empty.data[self.original.sampleRate*segment.start:self.original.sampleRate*segment.start + len(kick_sample.data)] += truncated
+                self.original.data[self.original.sampleRate*segment.start:self.original.sampleRate*segment.start + len(truncated)] *= 0.4
+                self.original.data[self.original.sampleRate*segment.start:self.original.sampleRate*segment.start + len(truncated)] += truncated
             last = segment.start
 
         last = 0
-        for segment in snares:
-            if last + len(snare_sample.data) > segment.start:
-                truncated = kick_sample.data
-                if self.original.sampleRate*segment.start + len(kick_sample.data) > len(self.original.data):
-                    truncated = truncated[:len(empty.data[self.original.sampleRate*segment.start:self.original.sampleRate*segment.start + len(kick_sample.data)])]
-                empty.data[self.original.sampleRate*segment.start:self.original.sampleRate*segment.start + len(snare_sample.data)] += snare_sample.data     
-            last = segment.start
         for segment in hats:
             if last + len(hat_sample.data) > segment.start:
-                truncated = kick_sample.data
+                truncated = hat_sample.data
                 if self.original.sampleRate*segment.start + len(kick_sample.data) > len(self.original.data):
-                    truncated = truncated[:len(empty.data[self.original.sampleRate*segment.start:self.original.sampleRate*segment.start + len(kick_sample.data)])]
-                empty.data[self.original.sampleRate*segment.start:self.original.sampleRate*segment.start + len(hat_sample.data)] += hat_sample.data
+                    truncated = truncated[:len(empty.data[self.original.sampleRate*segment.start:self.original.sampleRate*segment.start + len(hat_sample.data)])]
+                self.original.data[self.original.sampleRate*segment.start:self.original.sampleRate*segment.start + len(truncated)] *= 0.4
+                self.original.data[self.original.sampleRate*segment.start:self.original.sampleRate*segment.start + len(truncated)] += truncated
             last  = segment.start
+        last = 0
+        for segment in snares:
+            if last + len(snare_sample.data) > segment.start:
+                truncated = snare_sample.data
+                if self.original.sampleRate*segment.start + len(kick_sample.data) > len(self.original.data):
+                    truncated = truncated[:len(empty.data[self.original.sampleRate*segment.start:self.original.sampleRate*segment.start + len(snare_sample.data)])]
+                self.original.data[self.original.sampleRate*segment.start:self.original.sampleRate*segment.start + len(truncated)] *= 0.4
+                self.original.data[self.original.sampleRate*segment.start:self.original.sampleRate*segment.start + len(truncated)] += truncated     
+            last = segment.start
 
         self.log("Mastering...", 20)
 
@@ -98,7 +99,8 @@ class Beatbox(Remixer):
             data = make_stereo(self.original)
         else:
             data = self.original
-        audio.mix(empty, data, 0.5).encode(self.outfile)
+        #audio.mix(empty, data, 0.5).encode(self.outfile)
+        self.original.encode(self.outfile)
         self.updateTags(' (Beatbox Machine remix)')
         return self.outfile
 
